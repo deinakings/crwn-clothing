04ca488205a191f44cee089defc8da9f6374893b
deinakings <deinakings@gmail.com>
2021-03-26 19:27:51 -0300

    Add router and links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import Homepage from "./pages/homepage/homepage.component";
+import { Route } from "react-router-dom";
 
 export default function App() {
-  return <Homepage />;
+  return <Route exact={true} path="/" component={Homepage} />;
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
+  <button
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       style={{
         backgroundImage: `url(${imageUrl})`
@@ -13,7 +18,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
       <h1 className="title">{title}</h1>
       <span className="subtitle">Shop Now</span>
     </div>
-  </div>
+  </button>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
